c829fb687bd194f1f1c93e9958a640c6f768a7ae
子鵬 李 <hawk419383@gmail.com>
2020-11-13 08:47:54 +0800

    test

---
+++ js/storeaddr.js
@@ -0,0 +1,224 @@
+{
+    var store_cream = {
+        "北部": [{
+                city: '台北市',
+                store: [{
+                        StoreName: "鑫和洋行",
+                        Addres: "220 台北市信義區松山路465巷29弄19號",
+                        map: "https://goo.gl/maps/tE5oh7JSMiftHEM4A",
+                        phone: "0975 328 779"
+                    },
+
+                ]
+
+            }, {
+                city: '新北市',
+                store: [{
+                    StoreName: "大亞五金百貨商場",
+                    Addres: "247 新北市蘆洲區復興路111號",
+                    map: "https://goo.gl/maps/w6gqGyC663awM53m8",
+                    phone: "02 2283 9665"
+                }]
+            },
+            {
+                city: '宜蘭縣',
+                store: [{
+                    StoreName: "鑫和洋行",
+                    Addres: "260 宜蘭縣宜蘭市嵐峰路一段329號",
+                    map: "https://goo.gl/maps/As16pnJTZxBhimCo7",
+                    phone: "03 936 3306"
+                }]
+            }, {
+                city: '新竹市',
+                store: [{
+                    StoreName: "躍獅正大藥局",
+                    Addres: "300 新竹市北區中正路390號",
+                    map: "https://goo.gl/maps/NpwVGgbtrRxcy4wV8",
+                    phone: "03 535 6056"
+                }, {
+                    StoreName: "得安專業藥師藥",
+                    Addres: "300 新竹市東區中華路二段652號",
+                    map: "https://goo.gl/maps/3BcSLA8Xgy8Qf6tTA",
+                    phone: "03 528 3038"
+                }, {
+                    StoreName: "弘康醫療器材有限公司",
+                    Addres: "300 新竹市東區中正路404號",
+                    map: "https://goo.gl/maps/Zk7WpRgyUb4Hd2dm9",
+                    phone: "03 535 5506"
+                }]
+            }
+        ],
+
+
+
+        "中部": [{
+            city: '苗栗',
+            store: [{
+                StoreName: "欣香行",
+                Addres: "350 苗栗縣竹南鎮和興路345號",
+                map: "https://goo.gl/maps/QTUg33BWPsuupc9i7",
+                phone: "02 2283 9665"
+            }]
+        }],
+
+
+    };
+    var store_high = {
+        "北部": [{
+                city: '台北市',
+                store: [{
+                        StoreName: "鑫和洋行2222",
+                        Addres: "220 台北市信義區松山路465巷29弄19號",
+                        map: "https://goo.gl/maps/tE5oh7JSMiftHEM4A",
+                        phone: "0975 328 779"
+                    },
+                    {
+                        StoreName: "鑫和洋行2222",
+                        Addres: "220 台北市信義區松山路465巷29弄19號",
+                        map: "https://goo.gl/maps/tE5oh7JSMiftHEM4A",
+                        phone: "0975 328 779"
+                    }
+
+
+                ]
+
+            }, {
+                city: '新北市',
+                store: [{
+                    StoreName: "大亞五金百貨商場",
+                    Addres: "247 新北市蘆洲區復興路111號",
+                    map: "https://goo.gl/maps/w6gqGyC663awM53m8",
+                    phone: "02 2283 9665"
+                }]
+            },
+            {
+                city: '宜蘭縣',
+                store: [{
+                    StoreName: "鑫和洋行",
+                    Addres: "260 宜蘭縣宜蘭市嵐峰路一段329號",
+                    map: "https://goo.gl/maps/As16pnJTZxBhimCo7",
+                    phone: "03 936 3306"
+                }]
+            }, {
+                city: '新竹市',
+                store: [{
+                    StoreName: "躍獅正大藥局",
+                    Addres: "300 新竹市北區中正路390號",
+                    map: "https://goo.gl/maps/NpwVGgbtrRxcy4wV8",
+                    phone: "03 535 6056"
+                }, {
+                    StoreName: "得安專業藥師藥",
+                    Addres: "300 新竹市東區中華路二段652號",
+                    map: "https://goo.gl/maps/3BcSLA8Xgy8Qf6tTA",
+                    phone: "03 528 3038"
+                }, {
+                    StoreName: "弘康醫療器材有限公司",
+                    Addres: "300 新竹市東區中正路404號",
+                    map: "https://goo.gl/maps/Zk7WpRgyUb4Hd2dm9",
+                    phone: "03 535 5506"
+                }]
+            }
+        ],
+
+
+
+        "中部": [{
+            city: '苗栗',
+            store: [{
+                StoreName: "欣香行",
+                Addres: "350 苗栗縣竹南鎮和興路345號",
+                map: "https://goo.gl/maps/QTUg33BWPsuupc9i7",
+                phone: "02 2283 9665"
+            }]
+        }],
+        "南部": [{
+            city: '苗栗',
+            store: [{
+                StoreName: "欣香行",
+                Addres: "350 苗栗縣竹南鎮和興路345號",
+                map: "https://goo.gl/maps/QTUg33BWPsuupc9i7",
+                phone: "02 2283 9665"
+            }]
+        }],
+
+
+    };
+
+    var product = [{
+            img: "img/ointment.jpg",
+            name: "舒涼膏",
+            price: "120",
+            info: "Le DERMA 舒涼膏主用於緩和蚊蟲叮咬所造成的肌膚敏感不適(包括：泛紅、腫脹及發癢)，採溫和的草本配方設計，協助您舒緩蚊蟲叮咬後的諸多肌膚不適。",
+            ename: "ointmemt"
+
+        },
+        {
+            img: "img/LeDermaCream_Box&Cream_Reflection.png",
+            name: "舒緩護膚霜",
+            price: "800",
+            info: "Le DERMA 舒緩護膚霜採用溫和草本複方精華，所設計而成的舒敏緩癢潤膚品，不含有類固醇，特別針對敏弱肌膚體質及皮膚乾癢的族群，緩和各種肌膚不適。",
+            ename: "cream"
+        },
+        {
+            img: "img/high-efficiency-AD-soonthing-moisturizer2.jpg",
+            name: "異膚高效舒緩乳",
+            price: "1380",
+            info: "Le DERMA 源自草本精萃技術，輔以專利組合配方，再結合西方的皮膚科學，透過基礎實驗驗證功效，進而發展出系列草本精萃舒緩護膚品",
+            ename: "high"
+        },
+
+
+
+    ];
+
+
+
+    var data_group = {};
+
+
+    var app = new Vue({
+        el: '#app',
+        data() {
+            return {
+                show: '',
+                selected: '',
+                selected_item: '',
+                aera: '',
+                product: product
+            }
+        },
+        computed: {
+            city: function () {
+                return data_group[this.selected];
+            }
+        },
+        methods: {
+            detailsChange: function () {
+                //reset       
+                this.selected_item = '';
+
+            },
+            selectproduct: function (x) {
+                this.selected_item.store = '';
+                this.selected = '';
+                if (x == 'ointment' || x == 'cream') {
+                    this.selected_item = '';
+                    data_group = '';
+                    data_group = store_cream;
+                    this.aera = Object.keys(data_group)
+                    console.log(`${this.aera}`);
+
+                } else if (x == 'high') {
+                    this.selected = '';
+                    data_group = '';
+                    // data_group = store_high;
+                    // this.aera = Object.keys(data_group);
+                    // console.log(`${this.aera}`);
+                    alert("敬請期待")
+
+                }
+                this.show = true;
+            }
+        }
+    });
+}
